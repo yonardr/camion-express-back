738f0138dc77c06855f85005d9b12ee47ba61250
yonardr <rustem2129@mail.ru>
2024-11-05 20:09:29 +0300

    >>>

--- src/emailer/emailer.service.ts
+++ src/emailer/emailer.service.ts
@@ -9,8 +9,8 @@ export class EmailerService {
         console.log(file)
         const res = await this.mailerService
             .sendMail({
-                //to:  ['astron0408@gmail.com', 'astron71@mail.ru'], // list of receivers
-                to:  ['rustem2129@mail.ru'], // list of receivers
+                to:  ['astron0408@gmail.com', 'astron71@mail.ru'], // list of receivers
+                //to:  ['rustem2129@mail.ru'], // list of receivers
                 from: 'info@kamion-express.tmweb.ru', // sender address
                 subject: 'Заявка', // Subject line
                 template: 'application-approved',
@@ -27,8 +27,8 @@ export class EmailerService {
     async send(dto: GetDataMailDto) {
         const res = await this.mailerService
             .sendMail({
-                //to:  ['astron0408@gmail.com', 'astron71@mail.ru'], // list of receivers
-                to:  ['rustem2129@mail.ru'],
+                to:  ['astron0408@gmail.com', 'astron71@mail.ru'], // list of receivers
+                //to:  ['rustem2129@mail.ru'],
                 from: 'info@kamion-express.tmweb.ru', // sender address
                 subject: 'Заявка', // Subject line
                 template: 'application-approved',
@@ -38,8 +38,8 @@ export class EmailerService {
     }
     async sendWithFile(dto: GetDataMailDto, fileBuffer: Buffer) {
         return this.mailerService.sendMail({
-            //to: ['astron0408@gmail.com', 'astron71@mail.ru'],
-            to:  ['rustem2129@mail.ru'],
+            to: ['astron0408@gmail.com', 'astron71@mail.ru'],
+            //to:  ['rustem2129@mail.ru'],
             from: 'info@kamion-express.tmweb.ru',
             subject: 'Заявка',
             html: `<h1>${dto.name} оставил заявку</h1><h1>Номер телефона: ${dto.tel}</h1><h1>Почта этого пользователя: ${dto.email}</h1>`,
